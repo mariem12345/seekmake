c901c55739a36d77b005978cd4a8b4aa4063bf1d
mariem12345 <mariemah96@gmail.com>
2019-11-25 16:54:57 +0100

    fix admin seeder

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -11,6 +11,6 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        \App\Admin::updateOrCreate(['username'=>'admin'],['username'=>'admin','password'=>'admin','status='>'1']);
+        \App\Admin::updateOrCreate(['username'=>'admin'],['username'=>'admin','password'=>'admin','status'=>'1']);
     }
 }
